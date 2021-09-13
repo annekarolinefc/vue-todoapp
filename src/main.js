e610c37715e0332f75f359565e5445adcaeaa269
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2021-09-13 07:26:42 -0300

    Importando as bibliotecas no projeto

--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+//Importando a bliblioteca do PrimeVue
+import PrimeVue from "primevue/config";
+//Importando o Tema da aplicação
+import "primevue/resources/themes/saga-blue";
+//Importando o CSS
+import "primevue/resources/primevue.min.css";
+//Importando os icones
+import "primeicons/primeicons.css";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(PrimeVue)
+//o ID de onde será montado todo o projeto -> visualizar o arquivo index.html
+app.mount('#app')
