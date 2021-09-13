1286ce0e1866b791c2cc4ee9c76c6d3e3f62de77
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2021-09-13 07:29:19 -0300

    Importando os componentes do primevue

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 //Importando a bliblioteca do PrimeVue
 import PrimeVue from "primevue/config";
+//Importando os componentes 
+import Button from "primevue/button"
+import InputText from "primevue/inputtext";
+
 //Importando o Tema da aplicação
 import "primevue/resources/themes/saga-blue";
 //Importando o CSS
@@ -10,6 +14,9 @@ import "primevue/resources/primevue.min.css";
 import "primeicons/primeicons.css";
 
 const app = createApp(App);
+//Importando os componentes
+app.component("Button", Button);
+app.component("InputText", InputText)
 app.use(PrimeVue)
 //o ID de onde será montado todo o projeto -> visualizar o arquivo index.html
 app.mount('#app')
